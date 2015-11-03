22a894c3521cbd49c5d2711ab4c9b8978f0713fe
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2015-11-03 05:08:56 +0000

    Updated hue settings.

--- HueSunriseLamp.py
+++ HueSunriseLamp.py
@@ -21,12 +21,12 @@ import time
 from phue import Bridge
 
 # change to your IP address
-b = Bridge('192.168.x.x')
+b = Bridge('10.0.1.5')
 
 # turn the lamp on and make it as dim as possible
-b.set_light(1,'on', True)
-b.set_light(1, 'bri', 1)
-b.set_light(1, 'hue', 9977)
+b.set_light([1,2],'on', True)
+b.set_light([1,2], 'bri', 1)
+b.set_light([1,2], 'hue', 9977)
 
 # define the 5 different color transitions. Each variable runs for three minutes.
 orangeC =  {'transitiontime' : 1800, 'on' : True, 'bri' : 50, 'hue' : 9977}
@@ -36,22 +36,22 @@ yellowwhiteC =  {'transitiontime' : 1800, 'on' : True, 'bri' : 200, 'hue' : 1519
 whiteC =  {'transitiontime' : 1800, 'on' : True, 'bri' : 254, 'hue' : 38375} 
 
 # begin sunrinse lamp. This will last 
-b.set_light(1, orangeC)
+b.set_light([1,2], orangeC)
 print 'orange'
 time.sleep(180)
-b.set_light(1, orangeyellowC)
+b.set_light([1,2], orangeyellowC)
 print 'orange yellow'
 time.sleep(180)
-b.set_light(1, yellowC)
+b.set_light([1,2], yellowC)
 print 'yellow'
 time.sleep(180)
-b.set_light(1, yellowwhiteC)
+b.set_light([1,2], yellowwhiteC)
 print 'yellow white'
 time.sleep(180)
-b.set_light(1, whiteC)
+b.set_light([1,2], whiteC)
 time.sleep(180)
 print 'white'
 
-#delay for 1 hour then turn lamp off
-time.sleep(3600)
-b.set_light(1,'on', False)
+#delay for 1.5 hour then turn lamp off
+time.sleep(5400)
+b.set_light([1,2],'on', False)
